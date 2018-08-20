046e3e3ab23d7830a72c53193c9b18143c0ee542
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-08-20 15:18:41 +0530

    Added src code

---
+++ How to send message on whatsapp/How to send message on whatsapp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace How_to_send_message_on_whatsapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find your Account Sid and Token at twilio.com/console
+            const string accountSid = "ACbc58356cf0839e2541f69e59a2c64931";
+            const string authToken = "XXXXXXX";
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var message = MessageResource.Create(
+                body: "Hello there!",
+                from: new Twilio.Types.PhoneNumber("whatsapp:+XXXXX"),
+                to: new Twilio.Types.PhoneNumber("whatsapp:+XXxx")
+            );
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
